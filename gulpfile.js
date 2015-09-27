54970c3b2deb555d7ce78254ff196e1c03f521ad
renatn <zen@live.ru>
2015-09-27 18:23:54 +0300

    Added gulp dist task

--- gulpfile.js
+++ gulpfile.js
@@ -7,32 +7,57 @@ var source = require('vinyl-source-stream');
 var browserSync = require('browser-sync');
 var url = require('url');
 var proxy = require('proxy-middleware');
+var del = require('del');
+var runSequence = require('run-sequence');
+var gulpIf = require('gulp-if');
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
 
-var bundler = watchify(browserify({entries: './src/app.jsx', extensions: ['.jsx'], debug: true}));
+function bundle(watch) {
+    var bundler = browserify({entries: './src/app.jsx', extensions: ['.jsx'], debug: true});
 
-// Babel transform
-bundler.transform(babelify.configure({
-    sourceMapRelative: 'src/js'
-}));
+    if (watch) {
+        bundler = watchify(bundler);
+        bundler.on('update', rebundle);
+    }
 
-// On updates recompile
-bundler.on('update', build);
+    bundler.transform(babelify.configure({
+        sourceMapRelative: 'src/js'
+    }));
 
-function build() {
-    gutil.log('Compiling JSX...');
-    return bundler.bundle()
-        .on('error', function (err) {
-            gutil.log(err.message);
-            browserSync.notify("Browserify Error!");
-            this.emit("end");
-        })
-        .pipe(source('app.built.js'))
-        .pipe(gulp.dest('./src/js'))
-        .pipe(browserSync.stream({once: true}));
+    function rebundle() {
+        gutil.log('Compiling JSX...');
+        return bundler.bundle()
+            .on('error', function (err) {
+                gutil.log(err.message);
+                browserSync.notify("Browserify Error!");
+                this.emit("end");
+            })
+            .pipe(source('app.built.js'))
+            .pipe(gulp.dest('./dist/js'))
+            .pipe(gulpIf(watch, browserSync.stream({once: true})));
+    }
+
+    return rebundle();
 }
 
-gulp.task('build', function() {
-    return build();
+gulp.task('build:watch', function() {    
+    return bundle(true);
+});
+
+gulp.task('build:dist', function() {    
+    return bundle(false);
+});
+
+gulp.task('useref', function(){
+  var assets = useref.assets();
+
+  return gulp.src('./src/index.html')
+    .pipe(assets)
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(assets.restore())
+    .pipe(useref())
+    .pipe(gulp.dest('dist'))
 });
 
 gulp.task('browserSync', function() {
@@ -41,16 +66,24 @@ gulp.task('browserSync', function() {
 
     browserSync({
         server: {
-          baseDir: ['src', 'node_modules'],
+          baseDir: ['dist'],
           middleware: [proxy(proxyOptions)]
         },
     })
 })
 
-gulp.task('watch', ['build', 'browserSync'], function () {
+gulp.task('server', ['build:watch', 'browserSync'], function () {
     gulp.watch('./src/index.html', browserSync.reload);
 });
 
+gulp.task('clean', function() {
+  del('dist');
+})
+
+gulp.task('dist', ['build:dist', 'useref'], function() {
+    return gulp.src('./src/images/**/*.*')
+        .pipe(gulp.dest('dist/images'))
+});
 
 
-gulp.task('default', ['watch']);
+gulp.task('default', ['dist']);
